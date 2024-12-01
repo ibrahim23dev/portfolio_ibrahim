7648599b301423a913b9bc985c8c850955a35e7d
ibrahim23dev <ibrahim.soft.dev23@gmail.com>
2024-12-01 12:41:05 +0600

    update

--- src/assets/index.js
+++ src/assets/index.js
@@ -21,8 +21,8 @@ import tailwind from "./tech/tailwind.png";
 import typescript from "./tech/typescript.png";
 import threejs from "./tech/threejs.svg";
 
-import meta from "./company/meta.png";
-import shopify from "./company/shopify.png";
+import meta from "./company/wenexus.png";
+import shopify from "./company/HMDC Logo.png";
 import starbucks from "./company/regnum.png";
 import tesla from "./company/kraftwerk (1).svg";
 
